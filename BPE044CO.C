ac7da788446125d35a40179a1d3e5cd8185f419f
Apurv-Prakash <26bpe044@spt.pdpu.ac.in>
2026-09-02 11:48:01 +0530

    Add files via upload

---
+++ BPE044CO.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+int a,b;
+printf("Enter 2 values");
+scanf("%d %d", &a,&b);
+
+if (a>b);
+    print("%d > %d\n",a,b);
+else
+if (a<b);
+    printf("%d < %d",b,a);
+else
+   printf("%d = %d",a,b);
+
+   getch();
+
+return 0:
+}
